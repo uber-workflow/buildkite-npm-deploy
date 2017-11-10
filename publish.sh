9e2053dead2c21a367c1892e33ae4474a4bda268
Kevin Grandon <kevingrandon@yahoo.com>
2017-11-10 11:18:14 -0800

    Run yarn before running prepublish.
    Previously we were not running yarn and the transpile step would silently exit.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
-cd deploy_repo && npm-publish-prerelease
+cd deploy_repo && \
+	yarn && \
+	npm-publish-prerelease
